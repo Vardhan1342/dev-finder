6631a99fc3c5bff5126d4f28c267e55db5351d05
sri vardhan <srivardhan3566@gmail.com>
2024-04-30 14:45:33 +0530

    message

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,7 @@ import {  useRouter } from "next/navigation";
 export default function Home() {
   const session =useSession();
   const router=useRouter()
-  if(session.data?.user){
-    router.push("/home");
-  }
+  
   return (
     <main className="mt-10">
           
